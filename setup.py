3df9eea5fce8a4145a81aac50a36ed23ccf395ce
Jakub Bartczuk <bartczukkuba@gmail.com>
2019-09-18 15:26:38 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='imagenet_classes',
+    version='0.1',
+    url='https://github.com/lambdaofgod/imagenet_classes',
+    author='Jakub Bartczuk', 
+    packages=find_packages()
+)
